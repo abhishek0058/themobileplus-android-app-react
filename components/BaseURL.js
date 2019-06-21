57f4277105055d59d1713b59c442e4524be2ab16
Abhishek Sharma <abhishek0058@gmail.com>
2019-06-21 23:08:33 +0530

    base url updated

--- components/BaseURL.js
+++ components/BaseURL.js
@@ -1,2 +1,2 @@
-export default BaseURL = `http://192.168.43.68:8000`;
-// export default BaseURL = `http://themobileplus.co.in`;
+// export default BaseURL = `http://192.168.43.68:8000`;
+export default BaseURL = `http://themobileplus.co.in`;
